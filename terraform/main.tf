2ea916e85ef079342b49d9ff843c6d865ac66db5
Richard Potter <potter@axsh.net>
2018-04-02 20:58:48 +0900

    Added minimal RDS terraform that starts an RDS instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,13 @@ module "web2vm" {
   private_ip="${var.web2_server_private_ip}"
   web_server_eip_id="${var.web2_server_eip_id}"
 }
+
+module "rdsvm" {
+  source = "./therds"
+  aws_region="${var.aws_region}"
+  aws_availability_zone="${var.aws_availability_zone}"
+  subnet_external_id = "${module.thevpc.subnet_external_id}"
+  sgroup_id = "${module.thevpc.external_sg_id}"
+  private_ip="${var.web2_server_private_ip}"
+  web_server_eip_id="${var.web2_server_eip_id}"
+}
